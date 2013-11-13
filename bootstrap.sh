229f48f3ae4f48106a67001031986097e52d3c82
Charles Lirsac <c.lirsa@gmail.com>
2013-11-13 18:51:53 +0100

    Added install files options to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,42 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")"
+echo
+echo "DOTFILES INSTALL SCRIPT"
+echo "-----------------------"
+echo
 
+cd "$(dirname "${BASH_SOURCE}")"
+echo " => Update from git"
 git pull origin master
+echo
 
 function doIt() {
+  echo "=> Started copying files to your home directory"
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
 	source ~/.bash_profile
+  echo "=> Finished with sourced bash_profile"
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	read -p "=> This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
 	fi
 fi
 unset doIt
+echo
+
+for file in .installs/*
+do
+  filename=`echo $file | cut -d '/' -f 2`
+  read -p "=> Do you want to apply the $filename install script ? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    . ./$file
+  fi
+  echo
+done
