82fbc936d2fa529aa3b8829365f96d96e0fc71d0
lirsacc <c.lirsac@gmail.com>
2015-03-29 17:42:46 +0100

    Clean up bootrstap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-# Define help text
-usage="$(basename "$0") [-hfl] -- Bootstraping dotfiles\n\n
+# Usage
+# ==============================================================================
+
+usage="$(basename "$0") [-hfls] -- Bootstraping dotfiles\n\n
   -h  help\n
   -f  force overwrite files in user's home directory\n
+  -s  skip instal files (run them manually afterwards)\n
   -l  use local repo and do not update from git"
 
-# Read cli args and set system specific flags
+# Setup
+# ==============================================================================
+
 OPTIND=1
 
 force=false
-update=true
+pull=true
+skip=false
+installs_path='./.installs'
 
-while getopts "hfl?:" opt; do
+while getopts "hfls?:" opt; do
   case "$opt" in
   h|\?)
     echo -e $usage && echo && exit 0
@@ -21,71 +28,84 @@ while getopts "hfl?:" opt; do
     force=true
     ;;
   l)
-    update=false
+    pull=false
+    ;;
+  s)
+    skip=true
     ;;
   esac
 done
 
-echo
-echo "Running Dotfiles Install Script"
-echo "------------------------------------------------------------------------"
-echo
-
 shift $((OPTIND-1))
 [ "$1" = "--" ] && shift
 
-UNAME=$(uname)
-[[ "$UNAME" == "Darwin" ]] && osx=true || osx=false
-
-# Update dotfiles from git repo
-if [ $update == true ]; then
-  cd "$(dirname "${BASH_SOURCE}")"
-  echo " * Update from git"
-  git pull origin master
-  echo
-fi
+[[ $(uname) == "Darwin" ]] && osx=true || osx=false
 
 # Rsync the config files from git directory to home directory
-function doIt() {
-  echo " * Rsyncing files to your home directory..."
+function _rsync() {
+  echo " * rsync'ing files to your home directory..."
     rsync \
     --exclude ".git/" --exclude ".installs/" --exclude ".DS_Store" \
     --exclude "bootstrap.sh" --exclude "README.md" --exclude "LICENSE-MIT.txt" \
     -av --no-perms . ~
 }
 
-# Pass the -f or --force flag to force skip confirmation
-if [ $force == true ]; then
-    doIt
-else
-    read -p " * This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt
-    else
-      echo
-    fi
+# Install homebrew
+function _homebrew() {
+  echo " * Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
+# Bootstrap dotfiles
+# ==============================================================================
+
+echo
+echo "Running dotfiles bootstraping script"
+echo "------------------------------------------------------------------------"
+echo
+
+# Update dotfiles from git repo
+if [ $pull == true ]; then
+  cd "$(dirname "${BASH_SOURCE}")"
+  echo " * Update from git"
+  git pull origin master
+  echo
 fi
 
-unset doIt
+if [ $force ]; then
+    _rsync
+else
+    read -p " * This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    [[ $REPLY =~ ^[Yy]$ ]] && _rsync || echo
+fi
 
-for file in .installs/*
-do
-  filename=`echo $file | cut -d '/' -f 2`
-
-  # Skip osx specific install files if uname is not Darwin
-  if [[ $osx == false ]] && [[ $filename =~ 'osx' ]]; then
-    continue
-  fi
-
-  if [[ $force == true ]]; then
-    . ./$file
+if [ ! $(which brew) -a $osx ]; then
+  if [ $force ]; then
+    _homebrew
   else
-    read -p " * Do you want to apply the $filename install script ? (y/n) " -n 1
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-      . ./$file
-    fi
+    read -p " * Do you want to install homebrew ? (y/n) " -n 1
+    if [[ $REPLY =~ ^[Yy]$ ]] && _homebrew || echo
   fi
-  echo
-done
+fi
+
+if [ ! $skip ]; then
+  for file in $installs_path/*
+  do
+    filename=`echo $file | cut -d '/' -f 2`
+
+    # Skip osx specific install files if uname is not Darwin
+    [[ $osx == false ]] && [[ $filename =~ 'osx' ]] && continue
+
+    if [[ $force == true ]]; then
+      . ./$file
+    else
+      read -p " * Do you want to apply the $filename install script ? (y/n) " -n 1
+      if [[ $REPLY =~ ^[Yy]$ ]] && . ./$file
+    fi
+    echo
+  done
+fi
+
+unset _rsync _homebrew force pull skip osx
 
 source ~/.bash_profile
