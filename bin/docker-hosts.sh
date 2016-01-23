3bbe4cd6fa256b5d936a648bdf8987016165462a
lirsacc <c.lirsac@gmail.com>
2016-01-23 16:30:57 +0100

    Document docker-hosts cmd

--- bin/docker-hosts.sh
+++ bin/docker-hosts.sh
@@ -1,9 +1,18 @@
-running=( $(docker-machine ls | grep Running | awk '{ print $1 }') )
+#! /usr/bin/env bash
+
+# Populate host file with the ip of the running docker machines
+# A machine with name `machine` will be aliased as `machine.docker`
+
+running=($(docker-machine ls | grep Running | awk '{ print $1 }'))
+
+# Ask for the administrator password upfront (/etc/hosts requires it)
+sudo -v
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 for i in "${running[@]}"; do
     ip=$(docker-machine ip $i)
-    #remove line matching ip
-    sudo sed -i '/'$ip'/d' /etc/hosts
-    #insert ip/host on last line
-    echo "$ip $i.docker" | sudo tee -a /etc/hosts
+    echo "Adding $ip for machine $i"
+    sed -i '/'$ip'/d' /etc/hosts  # remove line matching ip
+    echo "$ip $i.docker" | sudo tee -a /etc/hosts  #insert ip/host on last line
 done
