9bcb43eb0f06979027c7762b86af31b4e550ed1d
lirsacc <c.lirsac@gmail.com>
2016-01-24 12:12:10 +0100

    Restore -s option in bootstrap script + fix shellcheck warnings

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,11 @@
 # Usage
 # ==============================================================================
 
-usage="$(basename "$0") [-hfls] -- Bootstraping dotfiles\n\n
-  -h  help\n
-  -f  force overwrite files in user's home directory\n
-  -l  use local repo only and do not update from git"
+usage="$(basename "$0") [-hfls] -- Bootstraping dotfiles\n
+  -h  help
+  -f  force overwrite files in user's home directory
+  -l  use local repo only and do not update from git
+  -s  rsync only"
 
 # Setup
 # ==============================================================================
@@ -15,6 +16,7 @@ OPTIND=1
 
 force=false
 pull=true
+sync_only=false
 
 scripts="install"
 directories=("projects")
@@ -22,13 +24,16 @@ directories=("projects")
 while getopts "hfls?:" opt; do
   case "$opt" in
   h|\?)
-    echo -e $usage && echo && exit 0
+    echo -e "$usage" && echo && exit 0
     ;;
   f)
     force=true
     ;;
   l)
     pull=false
+    ;;
+  s)
+    sync_only=true
     ;;
   esac
 done
@@ -40,7 +45,7 @@ shift $((OPTIND-1))
 
 # Rsync the config files from git directory to home directory
 function _rsync() {
-  echo " * rsync'ing files to your home directory..."
+  echo "rsync'ing files to your home directory..."
     rsync \
     --exclude ".git/" --exclude "./$scripts" --exclude ".DS_Store" \
     --exclude "bootstrap.sh" --exclude "README.md" --exclude "LICENSE-MIT.txt" \
@@ -49,8 +54,13 @@ function _rsync() {
 
 # Install homebrew
 function install_homebrew() {
-  echo " * Installing homebrew..."
+  echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
+function cleanup () {
+  unset _rsync install_homebrew force pull skip osx
+  rm -rf "${HOME:?}/$scripts"
 }
 
 # Bootstrap dotfiles
@@ -63,8 +73,9 @@ echo
 
 # Update dotfiles from git repo
 if $pull; then
+  # shellcheck disable=SC2128
   cd "$(dirname "${BASH_SOURCE}")" || exit 1
-  echo " * Update from git"
+  echo "Update from git"
   git pull origin master
   echo
 fi
@@ -77,8 +88,14 @@ done
 if $force; then
     _rsync
 else
-    read -p " * This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    read -rp "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
     [[ $REPLY =~ ^[Yy]$ ]] && _rsync || echo
+fi
+
+# Exit early
+if $sync_only; then
+  cleanup
+  exit 0
 fi
 
 # Homebrew install
@@ -86,7 +103,7 @@ if [[ -z $(which brew) ]] && $osx; then
   if $force; then
     install_homebrew
   else
-    read -p " * Do you want to install homebrew ? (y/n) " -n 1
+    read -rp "Do you want to install homebrew ? (y/n) " -n 1
     [[ $REPLY =~ ^[Yy]$ ]] && install_homebrew || echo
   fi
 fi
@@ -96,20 +113,19 @@ if [[ ! -z $(which brew) ]] && $osx; then
     source .brewfile
     source .caskfile
   else
-    read -p " * Do you want to install homebrew recipes ? (y/n) " -n 1
-    [[ $REPLY =~ ^[Yy]$ ]] && . .brewfile || echo
-    read -p " * Do you want to install cask recipes ? (y/n) " -n 1
-    [[ $REPLY =~ ^[Yy]$ ]] && . .caskfile || echo
+    read -rp "Do you want to install homebrew recipes ? (y/n) " -n 1
+    [[ $REPLY =~ ^[Yy]$ ]] && source .brewfile || echo
+    read -rp "Do you want to install cask recipes ? (y/n) " -n 1
+    [[ $REPLY =~ ^[Yy]$ ]] && source .caskfile || echo
   fi
 fi
 
 for file in ./$scripts/*
 do
-  echo $file
   if [[ ! -f $file ]]; then
     continue
   fi
-  filename=$(echo $file | cut -d '/' -f 3 | cut -d '.' -f 1)
+  filename=$(echo "$file" | cut -d / -f 3 | cut -d '.' -f 1)
 
   # Skip osx specific install files if uname is not Darwin
   [[ ! $osx ]] && [[ $file =~ osx ]] && continue
@@ -118,12 +134,11 @@ do
     # shellcheck source=/dev/null
     . ./$file
   else
-    read -p " * Do you want to apply the $filename install script ? (y/n) " -n 1
+    read -rp "Do you want to apply the $filename install script ? (y/n) " -n 1
     # shellcheck source=/dev/null
     [[ $REPLY =~ ^[Yy]$ ]] && . ./$file
   fi
   echo
 done
 
-unset _rsync install_homebrew force pull skip osx
-rm -rf "$HOME/$scripts"
+cleanup
