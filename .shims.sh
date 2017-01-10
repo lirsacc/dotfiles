96adc4ec30c3abc3771d7a993878f068bca38804
lirsacc <c.lirsac@gmail.com>
2017-01-10 11:30:02 +0100

    Better lazy shims for nvm and virtualenvwrapper

--- .shims.sh
+++ .shims.sh
@@ -1,17 +1,14 @@
-function mkvirtualenv() {
-  # shellcheck disable=1091
-  source virtualenvwrapper.sh
-  mkvirtualenv "$@"
+# From https://github.com/creationix/nvm/issues/539#issuecomment-110643090
+# Creates a function which will source the given file on first call.
+# Removed the check on file existence for virtualenvwrapper.sh, trust that
+# I know what I am doing.
+
+lazy_source () {
+  eval "$1 () { echo 'Sourcing $2' && source $2 && $1 \$@ }"
 }
 
-function rmvirtualenv() {
-  # shellcheck disable=1091
-  source virtualenvwrapper.sh
-  rmvirtualenv "$@"
-}
+lazy_source mkvirtualenv "virtualenvwrapper.sh"
+lazy_source rmvirtualenv "virtualenvwrapper.sh"
+lazy_source workon "virtualenvwrapper.sh"
 
-function workon() {
-  # shellcheck disable=1091
-  source virtualenvwrapper.sh
-  workon "$@"
-}
+lazy_source nvm "${NVM_DIR}/nvm.sh"
