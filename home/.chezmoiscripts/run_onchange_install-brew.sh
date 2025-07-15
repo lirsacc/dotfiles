0f9ff42314cd8901186b01fb6621798dc5116049
Charles Lirsac <code@lirsac.com>
2025-07-15 22:32:41 +0100

    Update home/.chezmoiscripts/install-brew.sh Update home/.chezmoiscripts/install-fisher.sh Update home/.chezmoiscripts/install-mise.sh

--- home/.chezmoiscripts/run_onchange_install-brew.sh
+++ home/.chezmoiscripts/run_onchange_install-brew.sh
@@ -2,7 +2,7 @@
 
 set -eufo pipefail
 
-# Brewfile hash: {{ include ".config/Brewfile" | sha256sum }}
+# Brewfile hash: {{ include "~/.config/Brewfile" | sha256sum }}
 # MacOS build version: {{ output "sw_vers" "--buildVersion" }}
 brew bundle --global install
 brew update
--- home/.chezmoiscripts/run_onchange_install-fisher.sh
+++ home/.chezmoiscripts/run_onchange_install-fisher.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-# hash: {{ include ".config/fish/fish_plugins" | sha256sum }}
+# hash: {{ include "~/.config/fish/fish_plugins" | sha256sum }}
 fisher update
