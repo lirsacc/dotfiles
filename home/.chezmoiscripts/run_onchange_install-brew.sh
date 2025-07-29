131fdcfba660b58f45bdd635d29d1a3feb95c283
Charles Lirsac <code@lirsac.com>
2025-07-29 21:25:19 +0100

    Update home/.chezmoiscripts/install-brew.sh Update home/.chezmoiscripts/setup-osx.sh

--- home/.chezmoiscripts/run_onchange_install-brew.sh
+++ home/.chezmoiscripts/run_onchange_install-brew.sh
@@ -4,6 +4,16 @@ set -eufo pipefail
 
 # Brewfile hash: {{ include "~/.config/Brewfile" | sha256sum }}
 # MacOS build version: {{ output "sw_vers" "--buildVersion" }}
+
+export HOMEBREW_BAT=1
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_BUILD_FROM_SOURCE=0
+export HOMEBREW_NO_EMOJI=1
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_BUNDLE_FILE_GLOBAL="~/.config/Brewfile"
+export HOMEBREW_BUNDLE_DUMP_NO_VSCODE="1"
+export HOMEBREW_BUNDLE_MAS_SKIP="1"
+
 brew bundle --global install
 brew update
 brew upgrade
